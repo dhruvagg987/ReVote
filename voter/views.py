6c5568163c805a9a1041ed90c522137320124c0b
dhruv aggarwal <dhruv.1822it1051@kiet.edu>
2020-10-17 22:20:00 +0530

    ...

--- voter/views.py
+++ voter/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse,Http404
 from django.shortcuts import render
-
+from .models import Voter
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>hiyah !</h1>")
+
+def view_candidates(request,pk):
+    try:
+        obj = Voter.objects.get(pk=pk)
+    except Voter.DoesNotExist:
+        raise Http404
+    return HttpResponse(f"username : {obj.username}"+"<br>"+f"number : {obj.number}")
